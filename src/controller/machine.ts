9df4a17e54bbaac1d4fed1c574b4c068a370ebd1
Ahmed Eid <ahmedeid6842@gmail.com>
2025-07-31 15:12:15 +0300

    refactor: integrate Joi validation into machine routes and remove it from controllers

--- src/controller/machine.ts
+++ src/controller/machine.ts
@@ -1,6 +1,5 @@
-import { addMachineValidation, getMachineQueryValidation, getNearestMachineValidation } from "../validators/machine";
 import { createMachineService, getMachinesService, deleteMachineService, updateMachineService } from "../services/machine";
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 
 export const addMachineController = async (req: Request, res: Response) => {
     /**
@@ -10,10 +9,6 @@ export const addMachineController = async (req: Request, res: Response) => {
      * TODO: you can't add two machines with distance between them less than 2 kilometer
      * DONE: add the machine with it's long,lat
      */
-
-    const { error } = addMachineValidation(req.body);
-    if (error) return res.status(400).send(error.details);
-
     let savedMachine = await createMachineService(req.body);
     return res.status(201).send({ message: "machine added succesfully", machine: savedMachine })
 }
@@ -24,8 +19,6 @@ export const getMachineController = async (req: Request, res: Response) => {
      * DONE: if there is a request query passed then find a machin based on query - else return all machines
      *  DONE: at this endpoint if location property was set is find exact match not the nearest one.
      */
-    const { error } = getMachineQueryValidation(req.query);
-    if (error) return res.status(400).send(error.details)
 
     const machinesPerPage = 10, pageNumber = Number(req.query.page) || 1;
     delete req.query.page;
@@ -41,8 +34,6 @@ export const getNearestMachineController = async (req: Request, res: Response) =
      * DONE: validate incoming request parameter
      * DONE: find the nearest five machines near to that location
      */
-    const { error } = getNearestMachineValidation(req.params);
-    if (error) return res.status(400).send(error.details);
 
     const machines = await getMachinesService({ location: [req.params.longitude, req.params.latitude] }, true, true, true, 0, 5);
     if (!machines) return res.status(404).send({ message: "No machine found" });
@@ -58,8 +49,6 @@ export const updateMachineController = async (req: Request, res: Response) => {
      * DONE: check if the machine exist
      * DONE: update product with given ID
      */
-    const { error } = addMachineValidation(req.body, true);
-    if (error) return res.status(400).send(error.details);
 
     let updatedMachine = await updateMachineService({ _id: req.params.machineID }, { $set: req.body });
     if (!updatedMachine) return res.status(400).send({ message: "No machine found" });
--- src/routes/machine.ts
+++ src/routes/machine.ts
@@ -8,13 +8,15 @@ import {
 } from '../controller/machine';
 import { isAuthenticated } from "../middleware/isAuthenticated";
 import { role } from '../middleware/role';
+import { validate } from '../middleware/validate';
+import { MachineValidators } from '../validators/machine';
 
 const router = express.Router();
 
-router.post("/", isAuthenticated, role("admin"), addMachineController);
-router.get("/", getMachineController);
-router.get("/nearest/:longitude/:latitude", getNearestMachineController);
-router.put("/:machineID", isAuthenticated, role("admin"), updateMachineController);
+router.post("/", isAuthenticated, role("admin"), validate(MachineValidators.addMachineBody, 'body'),addMachineController);
+router.get("/", getMachineController, validate(MachineValidators.getMachineQuery,'query'));
+router.get("/nearest/:longitude/:latitude", validate(MachineValidators.getNearestMachineParams,'params'), getNearestMachineController);
+router.put("/:machineID", isAuthenticated, role("admin"), validate(MachineValidators.updateMachineBody,'body'),updateMachineController);
 router.delete("/:machineID", isAuthenticated, role("admin"), deleteMachineController);
 
 export const Machine = router;
